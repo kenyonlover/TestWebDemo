10ac420e6e7378b08509db20aa87dae014fe2a6b
wuchengyao <307980463@qq.com>
2019-02-18 16:35:23 +0800

    第一次在git提交整个项目

---
+++ src/com/test/TestGit.java
@@ -0,0 +1,14 @@
+package com.test;
+/**
+ * 作者：administrator
+ * 时间：2019年2月18日 下午3:49:11
+ * 说明：
+ */
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("测试git");
+	}
+
+}
